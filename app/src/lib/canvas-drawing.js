da3fcc6c05c1e006229ac97136cac2fe34f37fc0
Niaz Khan <fmoonlighth@gmail.com>
2024-04-04 23:23:45 +0500

    changed output icon

--- app/src/lib/canvas-drawing.js
+++ app/src/lib/canvas-drawing.js
@@ -36,10 +36,13 @@ function mapNodes(nodes) {
       "M30 5V45H50.47619c11.267908 0 20-9.000045 20-20s-8.732091-20-20-20H30z"
     ),
     notPath = new Path2D(
-      "M 28.9688 2.5938 v 44.8125 l 2.1563 -1.0625 l 41.0313 -20 v -1.3748 L 68.022 24.969 A 1 1 0 0 0 77.193 23.935 A 1 1 0 0 0 68.011 24.966 l 3.741 -1.591 z"
+      "M 28.9688 2.5938 v 44.4062 l 27.0312 -22 L 56 25 A 1 1 0 0 0 63 25 A 1 1 0 0 0 56 25 z"
     ),
     outputPath = new Path2D(
       "M2410 4433 c-455 -49 -881 -353 -1082 -773 -94 -197 -132 -366 -132 -590 0 -235 34 -387 134 -598 70 -147 146 -250 287 -388 161 -159 165 -167 173 -379 4 -124 10 -168 23 -196 23 -46 79 -103 118 -119 28 -12 29 -14 29 -85 0 -73 19 -146 45 -175 12 -14 10 -23 -15 -74 -23 -45 -30 -72 -30 -115 1 -60 19 -122 44 -149 14 -16 12 -23 -14 -76 -66 -130 -19 -286 104 -346 38 -18 45 -27 55 -67 28 -111 116 -213 228 -265 57 -27 83 -33 159 -36 111 -5 175 11 261 67 76 49 138 129 167 215 19 59 26 68 66 89 54 28 105 92 121 150 16 61 6 148 -25 203 -24 44 -25 48 -10 65 25 28 43 89 44 150 0 43 -7 70 -30 115 -25 51 -27 60 -15 74 26 29 45 102 45 175 0 71 1 73 29 85 39 16 95 73 118 119 13 28 19 72 24 201 7 207 16 226 152 352 560 516 597 1382 83 1938 -264 285 -630 444 -1011 438 -60 -1 -126 -3 -145 -5z m430 -201 c237 -63 420 -170 584 -342 338 -354 422 -880 212 -1326 -67 -142 -138 -237 -277 -374 -107 -105 -133 -137 -161 -195 -32 -68 -33 -74 -38 -237 -5 -159 -6 -168 -29 -192 l-24 -26 -547 0 -547 0 -24 26 c-23 24 -24 33 -29 192 -5 162 -6 169 -38 236 -27 58 -54 91 -160 195 -70 69 -149 157 -175 195 -144 206 -217 436 -217 681 0 313 123 616 340 834 192 194 401 301 690 355 14 2 102 3 195 1 130 -2 188 -8 245 -23z m140 -2931 c0 -52 -3 -62 -26 -80 -26 -20 -38 -21 -394 -21 -356 0 -368 1 -394 21 -23 18 -26 28 -26 80 l0 59 420 0 420 0 0 -59z m-25 -306 c32 -31 33 -80 3 -112 l-21 -23 -376 0 c-371 0 -375 0 -398 22 -32 30 -31 81 2 113 l24 25 371 0 371 0 24 -25z m6 -344 c25 -31 24 -79 -3 -108 l-21 -23 -376 0 c-371 0 -375 0 -398 22 -38 36 -27 109 20 128 12 5 188 8 390 7 l369 -2 19 -24z m-184 -353 c-28 -49 -100 -103 -160 -119 -99 -27 -224 28 -275 120 l-23 41 241 0 242 0 -25 -42z"
+    ),
+    inputPath = new Path2D(
+        "m 141.75 138.6 v 20.25 l 14.85 0 l 9.45 -10.8 l -9.9 -9.45 z"
     ),
     wireColor = "#093708"
   export function getLogicGateDiagram(tree) {
@@ -111,7 +114,7 @@ function mapNodes(nodes) {
           ("NOT" != t.circutType && "OUTPUT" != t.circutType) ||
             ((t.x = -e * depthSize), (t.y = n)),
           positionNodes(t.input, e + 1, n))
-        : "INPUT" == t.circutType && ((t.x = -e * depthSize), (t.y = n));
+        : "INPUT" == t.circutType && ((t.x = -600), (t.y = -100));
   }
   
   function drawLogicGates(node, ctx) {
@@ -119,6 +122,8 @@ function mapNodes(nodes) {
     
       ctx.fillStyle = '#c7c8a9' // greenish beige
       ctx.strokeStyle = '#6d0703' // dark red
+      ctx.lineJoin = "round"
+      ctx.lineCap = "round"
       switch (node.circutType) {
         case "AND":
           ctx.translate(node.x, node.y);
@@ -139,16 +144,36 @@ function mapNodes(nodes) {
           break;
     
         case "INPUT":
-          /////
-          let x = -500  
-          let y = node.inputVaraible == 'A' ? 100 : node.inputVaraible == 'B' ? 200 : node.inputVaraible == 'C' ? 300 : node.inputVaraible == 'C' ? 400 : -1500;
-          ctx.fillText(node.inputVaraible, x + 40, y + 35);
-          ctx.beginPath();
-          ctx.arc(x + 50, y + 25, 20, 0, 2 * Math.PI);
-          ctx.lineWidth = 3;
-          ctx.stroke();
-          /////
+        //   /////
+        //   let x = -500  
+        //   let y = node.inputVaraible == 'A' ? 100 : node.inputVaraible == 'B' ? 200 : node.inputVaraible == 'C' ? 300 : node.inputVaraible == 'C' ? 400 : -1500;
+        //   ctx.fillText(node.inputVaraible, x + 40, y + 35);
+        //   ctx.beginPath();
+        //   ctx.arc(x + 50, y + 25, 20, 0, 2 * Math.PI);
+        //   ctx.lineWidth = 3;
+        //   ctx.stroke();
+        //   /////
           
+        ctx.save();
+          let x =  getSuitableX(node) - 95
+        //   let y = node.inputVaraible == 'A' ? 100 : node.inputVaraible == 'B' ? 200 : node.inputVaraible == 'C' ? 300 : node.inputVaraible == 'C' ? 400 : -1500;
+          let y = getSuitableY(node) - 124
+          ctx.fillStyle = "#0300c3"
+          ctx.strokeStyle = "#060352"
+        //   ctx.scale(0.4, 0.4)
+          ctx.translate(x, y);
+          ctx.fill(inputPath);
+          ctx.stroke(inputPath);
+          ctx.restore();
+          
+          ctx.fillStyle = "#ffffff"
+          ctx.font = "12px Arial"
+          ctx.fillText("0", x + 148 , y + 153);
+        //   ctx.beginPath();
+        //   ctx.arc(x + 50, y + 25, 20, 0, 2 * Math.PI);
+        //   ctx.lineWidth = 3;
+        //   ctx.stroke();  
+        
           // ctx.fillText(node.inputVaraible, node.x + 40, node.y + 35);
           // ctx.beginPath();
           // ctx.arc(node.x + 50, node.y + 25, 20, 0, 2 * Math.PI);
@@ -157,10 +182,25 @@ function mapNodes(nodes) {
           break;
     
         case "OUTPUT":
-          ctx.translate(node.x + 69, node.y + 26);
-          ctx.scale(0.015, 0.015);
-          ctx.rotate((180 * Math.PI) / 180);
-          ctx.fill(outputPath);
+            // ctx.translate(node.x + 69, node.y + 26);
+            // ctx.scale(0.015, 0.015);
+          
+            
+            let _x = 30, _y = 15
+            ctx.beginPath();
+            ctx.rect(node.x + _x, node.y + _y, 20, 20)
+            ctx.lineWidth = 2
+            ctx.fill();
+            ctx.stroke();
+            
+            ctx.save();
+            ctx.fillStyle = "#6d0703"
+            ctx.font = "bold 16px Arial"
+            ctx.fillText("?", node.x + _x + 5, node.y + _y + 15);
+            ctx.restore();
+          
+        //   ctx.rotate((180 * Math.PI) / 180);
+        //   ctx.fill(outputPath);
           break;
       }
     
@@ -171,7 +211,7 @@ function mapNodes(nodes) {
         if (node.leftInput.circutType !== 'INPUT') {
             drawBetweenNodes(node.leftInput, node, "leftInput", ctx);
           } else {
-                drawBetweenNodes({x: -500, y: getSuitableY(node.leftInput) , circutType: 'INPUT'}, node, "leftInput", ctx, getSuitableDistanceFromInput(node.leftInput));
+                drawBetweenNodes({x: getSuitableX(node.leftInput), y: getSuitableY(node.leftInput) , circutType: 'INPUT'}, node, "leftInput", ctx, getSuitableDistanceFromInput(node.leftInput));
         }
       }
     
@@ -180,7 +220,7 @@ function mapNodes(nodes) {
         if (node.rightInput.circutType !== 'INPUT') {
             drawBetweenNodes(node.rightInput, node, "rightInput", ctx);
         } else {
-          drawBetweenNodes({x: -500, y: getSuitableY(node.rightInput) , circutType: 'INPUT'}, node, "rightInput", ctx, getSuitableDistanceFromInput(node.rightInput));
+          drawBetweenNodes({x: getSuitableX(node.rightInput), y: getSuitableY(node.rightInput) , circutType: 'INPUT'}, node, "rightInput", ctx, getSuitableDistanceFromInput(node.rightInput));
         }
       }
     
@@ -189,7 +229,7 @@ function mapNodes(nodes) {
         if (node.input.circutType !== 'INPUT') {
             drawBetweenNodes(node.input, node, "input", ctx);
         } else {
-          drawBetweenNodes({x: -500, y: getSuitableY(node.input) , circutType: 'INPUT'}, node, "input", ctx, getSuitableDistanceFromInput(node.input));
+          drawBetweenNodes({x: getSuitableX(node.input), y: getSuitableY(node.input) , circutType: 'INPUT'}, node, "input", ctx, getSuitableDistanceFromInput(node.input));
         }
       }
   }
@@ -210,15 +250,31 @@ function mapNodes(nodes) {
               return null; // or throw new Error('Unknown inputVariable');
       }
   }
+  function getSuitableX(node) {
+      switch (node.inputVaraible) {
+          case 'A':
+              return -650 + 70;
+          case 'B':
+              return -650 + 30;
+          case 'C':
+              return -650 + 100;
+          case 'D':
+              return -650 + 40;
+          default:
+              // Handle the unexpected case or return a default value
+              console.error('Unknown inputVaraible:', node.inputVaraible);
+              return null; // or throw new Error('Unknown inputVariable');
+      }
+  }
   
   function getSuitableDistanceFromInput(node) {
       switch (node.inputVaraible) {
           case 'A':
-              return 10;
+              return 50;
           case 'B':
-              return 20;
+              return 10;
           case 'C':
-              return 30;
+              return 50;
           case 'D':
               return 40;
           default:
@@ -243,7 +299,7 @@ function mapNodes(nodes) {
           ctx.lineTo(x1 + firstXDistance, y2);
           ctx.lineTo(x2, y2);
           
-          ctx.lineWidth = 3;
+          ctx.lineWidth = 2.5;
           ctx.strokeStyle = wireColor
           ctx.stroke();
       }
@@ -304,13 +360,16 @@ function mapNodes(nodes) {
           (n1.circutType == "OR" && n2.circutType == "NOT")) {
           drawLine(x1, y1, x2, n2.y + 25, ctx, firstAbsoluteXDistance);
       } else if (n1.circutType == "INPUT" && n2.circutType == "AND" ||
-                 n1.circutType == "NOT" && n2.circutType == "AND" ||
                  n1.circutType == "AND" && n2.circutType == "AND" ||
                  n1.circutType == "AND" && n2.circutType == "OR" ||
                  n1.circutType == "INPUT" && n2.circutType == "OR" ||
-                 n1.circutType == "NOT" && n2.circutType == "OR" ||
                  n1.circutType == "OR" && n2.circutType == "OR" ||
                  n1.circutType == "OR" && n2.circutType == "AND") {
+          drawLine(x1 , y1, x2, y2, ctx, firstAbsoluteXDistance);
+        } else if (n1.circutType == "NOT" && n2.circutType == "AND" ||
+                   n1.circutType == "NOT" && n2.circutType == "OR")  {
+          drawLine(x1-6, y1, x2, y2, ctx, firstAbsoluteXDistance);
+        } else if (n2.circutType === 'OUTPUT') {
           drawLine(x1, y1, x2, y2, ctx, firstAbsoluteXDistance);
       }
   }
